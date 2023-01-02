9cc65ac54646bd5b8e76c158782347daedf8bf7f
Lucas Navarro <Lucasnav6@gmail.com>
2023-01-02 11:08:20 -0300

    🎨 format of the code

--- portfolio-frontend/tailwind.config.js
+++ portfolio-frontend/tailwind.config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    './src/**/*.{html,ts}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
