8673982799ce1174d289cf87f40d6ba629f987a8
Lucas Navarro <Lucasnav6@gmail.com>
2023-01-04 12:21:11 -0300

    ✨ Create button component

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ButtonPrimaryComponent } from './buttons/button-primary/button-primary.component';
+import { ButtonSecundaryComponent } from './buttons/button-secundary/button-secundary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ButtonPrimaryComponent,
+    ButtonSecundaryComponent,
   ],
   imports: [
     BrowserModule,
---
+++ portfolio-frontend/src/app/buttons/button-secundary/button-secundary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-secundary',
+  templateUrl: './button-secundary.component.html',
+  styleUrls: ['./button-secundary.component.css']
+})
+export class ButtonSecundaryComponent {
+
+}
