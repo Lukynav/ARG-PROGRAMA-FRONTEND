5d908729be963c1ce7701157ee3787995d894ebe
Lucas Navarro <Lucasnav6@gmail.com>
2023-01-07 16:12:31 -0300

    📁 Move component into a folder

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './header/header.component'
-import { ButtonPrimaryComponent } from './buttons/button-primary/button-primary.component'
-import { ButtonSecundaryComponent } from './buttons/button-secundary/button-secundary.component'
-import { SkillsAndServicesComponent } from './skills-and-services/skills-and-services.component';
-import { CardComponent } from './card/card.component'
+import { HeaderComponent } from './components/header/header.component'
+import { ButtonPrimaryComponent } from './components/buttons/button-primary/button-primary.component'
+import { ButtonSecundaryComponent } from './components/buttons/button-secundary/button-secundary.component'
+import { SkillsAndServicesComponent } from './components/skills-and-services/skills-and-services.component'
+import { CardComponent } from './components/card/card.component'
 
 @NgModule({
   declarations: [
